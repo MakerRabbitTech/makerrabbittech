83a7ed0cbcfd2cc99b0283e8ef47cf26211a3fa7
MakerRabbitTech <makerrabbittech@gmail.com>
2025-01-25 22:00:24 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import MenuComponent from './App'; // 更名為正確的 MenuComponent
+import App from './App'; // 更名為正確的 MenuComponent
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename="/makerrabbittech">
-    <MenuComponent />
+    <App />
   </BrowserRouter>
 );
 
